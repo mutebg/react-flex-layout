687f0cb1b947caf6c29682d46ef0bab4c6465d8f
Stoyan Delev <s.delev@elements.nl>
2020-12-15 14:44:24 +0200

    Add alias

---
+++ src/react.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+const {
+  cloneElement,
+  createContext,
+  Component,
+  createElement,
+  forwardRef,
+} = React;
+
+export { cloneElement, createContext, Component, createElement, forwardRef };
